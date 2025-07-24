e1f63edbabc11b9ee980f9a2fa0cfc0e2689afbe
ParasuramanA <parasuapt02@gmail.com>
2025-07-24 21:05:37 +0530

    minor schema update

--- models/user.js
+++ models/user.js
@@ -1,35 +1,35 @@
-const {DataTypes} = require("sequelize")
-const sequelize = require("../config/db")
-
-
-const users = sequelize.define('users', 
+const users = sequelize.define(
+  'users',
   {
     firstname: {
       type: DataTypes.STRING,
       allowNull: false,
+      field: 'firstname', // explicitly mapped
     },
     lastname: {
       type: DataTypes.STRING,
       allowNull: false,
+      field: 'lastname',
     },
     email: {
       type: DataTypes.STRING,
       unique: true,
       allowNull: false,
+      field: 'email',
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
+      field: 'password',
     },
     role: {
       type: DataTypes.ENUM('admin', 'teacher', 'student', 'parent'),
       defaultValue: 'student',
+      field: 'role',
     },
-  }, 
+  },
   {
     timestamps: true,
+    tableName: 'users', 
   }
 );
-  
-
-module.exports = users
